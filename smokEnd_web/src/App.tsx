dc2034c999f7d6cdc1adb47f7eb841b699df3a2d
ChoiSuJin03 <106504517+ChoiSuJin03@users.noreply.github.com>
2024-05-23 00:12:52 +0900

    수정

--- smokEnd_web/src/App.tsx
+++ smokEnd_web/src/App.tsx
@@ -4,12 +4,18 @@ import Signin from "./pages/Signin";
 import Findpw from "./pages/Findpw";
 import Signup from "./pages/Signup";
 import Review from "./pages/Review";
-import SmokeText from "./pages/SmokeText";
 import Introduction from "./pages/Introduction";
-
+// import SelfAssessment from "./pages/SelfAssessment";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import SmokeText from "./pages/SmokeText";
+// import NicotineComponent from "./components/NicotineComponent";
+// import HabitComponent from "./components/HabitComponent";
+// import KnowledgeComponent from "./components/KnowledgeComponent";
+// import ConditionComponent from "./components/ConditionComponent";
+import Footer from "./components/Footer";
+// import SelfAssessmentResult from "./components/SelfAssessmentResult";
 
 const GlobalStyles = createGlobalStyle`
   *{
@@ -56,6 +62,13 @@ function App() {
           <Route path="/login" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/findpw" element={<Findpw />} />
+          {/* <Route path="/selfAssessment/*" element={<><SelfAssessment /></>}> */}
+          {/* <Route path="nicotine" element={<><NicotineComponent/></>} /> */}
+          {/* <Route path="habit" element={<HabitComponent/>}/> */}
+          {/* <Route path="knowledge" element={<KnowledgeComponent/>}/> */}
+          {/* <Route path="condition" element={<ConditionComponent/>}/> */}
+          {/* <Route path="result" element={<SelfAssessmentResult/>}/> */}
+
           <Route path="/Review" element={<Review />} />
           <Route path="/SmokeText" element={<SmokeText />} />
           <Route path="/Introduction" element={<Introduction />} />
